f5c00d6989913a515961742f99fd006232120c8d
itumeleng mokwena <itumelengmokwena75@gmail.com>
2022-06-30 14:06:11 -0700

    Hello, printf

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+
+
+
 /**
  *
  *  * main - Entry point
@@ -13,9 +16,8 @@ int main(void)
 
 {
 
-	  
-           puts("\"Programming is like building a multilingual puzzle");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	   return (0);
+			return (0);
 
-}	   
+}
